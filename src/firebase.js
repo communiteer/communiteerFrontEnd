a60394f3d47f20cb230e95e7094d9cb3ce9eb85b
BIWhitfield <beniwhitfield@gmail.com>
2017-07-27 08:40:08 +0100

    [firebase] initial commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// import firebase from 'firebase';
+
+// const config = {
+// 			apiKey: "AIzaSyC1LvC3eXq7yv05jB8JM1XL410Yj3qTU3s",
+// 			authDomain: "communiteer-25edb.firebaseapp.com",
+// 			databaseURL: "https://communiteer-25edb.firebaseio.com",
+// 			projectId: "communiteer-25edb",
+// 			storageBucket: "",
+// 			messagingSenderId: "691945375452"
+// 		};
+	
+// firebase.initializeApp(config);
+
+// export default firebase;
