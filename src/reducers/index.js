48ffd3125b160339dc960040619a97849ccc6844
BIWhitfield <beniwhitfield@gmail.com>
2017-07-27 09:08:16 +0100

    [index(reducer)] initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+import { combineReducers } from 'redux';
+import UserProfileReducer from './UserProfileReducer';
+import GroupsInAreaReducer from './GroupsInAreaReducer';
+import EventsInAreaReducer from './EventsInAreaReducer';
+import DisplayGroupPageReducer from './DisplayGroupPageReducer';
+import DisplayEventSkillsReducer from './DisplayEventSkillsReducer';
+import AddGroupReducer from './AddGroupReducer';
+import AuthReducer from './AuthReducer';
+
+export default combineReducers({
+  user: UserProfileReducer,
+  groups: GroupsInAreaReducer,
+  events: EventsInAreaReducer,
+  group: DisplayGroupPageReducer,
+  eventSkills: DisplayEventSkillsReducer,
+  createGroupForm: AddGroupReducer,
+  auth: AuthReducer
+})
