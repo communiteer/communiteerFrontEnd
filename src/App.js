400d3b248dfa80ac7b7e391cd098d8a4b1c2b28d
BIWhitfield <beniwhitfield@gmail.com>
2017-07-27 08:39:50 +0100

    [App] Set up Redux and Router

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+	StyleSheet,
+	Text,
+	PixelRatio,
+	View
+} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux'
+import Thunk from 'redux-thunk';
+import reducers from './reducers';
+import RouterComponent from './Router';
+
+
+class App extends Component {
+
+	render () {
+	const store = createStore(reducers, {}, applyMiddleware(Thunk))
+		return (
+			<Provider store={store}>
+				<RouterComponent />
+			</Provider>
+		);
+	}
+}
+
+
+
+export default App;
+
